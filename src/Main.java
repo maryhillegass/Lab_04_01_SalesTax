b955117e673003459b062e16c576dcedabdf0c74
maryhillegass <maryhillegass@gmail.com>
2025-06-15 02:15:03 -0400

    Sales Tax calculated

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        double purchasePrice = 15.00;
+        double TAX_RATE = 0.05;
+        double salesTax = purchasePrice * TAX_RATE;
 
+        System.out.println("For a purchase price of " + purchasePrice
+                + " the 5% sales tax is " + salesTax);
         }
     }
-}
