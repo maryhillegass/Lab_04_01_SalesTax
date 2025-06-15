71e9fb0a83a91489dac8d929d62b9878a1691a02
maryhillegass <maryhillegass@gmail.com>
2025-06-15 02:52:07 -0400

    added final to the constant declaration

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         double purchasePrice = 15.00;
-        double TAX_RATE = 0.05;
+        final double TAX_RATE = 0.05;
         double salesTax = purchasePrice * TAX_RATE;
 
         System.out.println("For a purchase price of " + purchasePrice
